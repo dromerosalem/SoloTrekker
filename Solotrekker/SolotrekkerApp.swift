1153b1b4d65af527bab5a7d5a3e2ca76420022df
David <dromerosalem@gmail.com>
2025-04-12 22:11:54 +0100

    Initial Commit

---
+++ Solotrekker/SolotrekkerApp.swift
@@ -0,0 +1,17 @@
+//
+//  SolotrekkerApp.swift
+//  Solotrekker
+//
+//  Created by David Romero Salem on 12/04/2025.
+//
+
+import SwiftUI
+
+@main
+struct SolotrekkerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SolotrekkerTests/SolotrekkerTests.swift
@@ -0,0 +1,16 @@
+//
+//  SolotrekkerTests.swift
+//  SolotrekkerTests
+//
+//  Created by David Romero Salem on 12/04/2025.
+//
+
+import Testing
+
+struct SolotrekkerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
